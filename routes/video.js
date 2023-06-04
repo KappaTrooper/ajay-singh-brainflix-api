089ff29b624e2ee1466264ef853d441788cc46c2
Ajay Singh <contact@ajaysingh.ca>
2023-06-04 18:18:39 -0400

    add app.get with id and status message

--- routes/video.js
+++ routes/video.js
@@ -10,6 +10,7 @@ const videoDataPath = path.resolve('./data/videos.json');
 
 // GET /videos and return 4 items from json
 router.get('/', (req, res) => {
+  // read data/videos.json
   const videoData = JSON.parse(fs.readFileSync(videoDataPath));
 
   const videos = videoData.map((video) => {
@@ -25,5 +26,19 @@ router.get('/', (req, res) => {
   res.json(videos);
 });
 
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  // read data/videos.json
+  const videoData = JSON.parse(fs.readFileSync(videoDataPath));
+  const video = videoData.find((video) => video.id === id);
+
+  if (video) {
+    res.json(video);
+  } else {
+    // if wrong id send 404 status
+    res.status(404).json({ error: 'Video not found' });
+  }
+});
 
 export default router;
